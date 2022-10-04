f82962fed991791bfea7a045d82670730f75035a
junhui77 <uiuxdesigner@naver.com>
2022-10-04 12:22:31 +0900

    커밋

---
+++ src/test/java/com/simple/myweb/CloudWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.myweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
